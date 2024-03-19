c95c14956705fd980f2d1ff84b97f9f1d327747a
Fabiana-Nascimento <164015191+Fabiana-Nascimento@users.noreply.github.com>
2024-03-19 14:00:57 -0300

    Add files via upload

---
+++ comunidadeimpressionadora/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '7c3fabebbade10c80dc49b856f6c6ddd'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade_impressionadora.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-info'
+
+# Agora que o objeto SQLAlchemy está criado, importe os módulos de rotas
+from comunidadeimpressionadora import routes
---
+++ comunidadeimpressionadora/models.py
@@ -0,0 +1,30 @@
+from comunidadeimpressionadora import db, login_manager
+from datetime import datetime
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(id_usuario):
+    return Usuario.query.get(int(id_usuario))
+
+
+class Usuario(db.Model, UserMixin):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    senha = db.Column(db.String, nullable=False)
+    foto_perfil = db.Column(db.String, default='default.jpg')
+    posts = db.relationship('Post', backref='autor', lazy=True)
+    cursos = db.Column(db.String, nullable=False, default='Não Informado')
+
+    def contar_posts(self):
+        return len(self.posts)
+
+
+class Post(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String, nullable=False)
+    corpo = db.Column(db.Text, nullable=False)
+    data_criacao = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
